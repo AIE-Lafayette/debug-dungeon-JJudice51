2aa1a756f5d82a7613f34fae000c3fbce75df0aa
Justin Judice <jjudice51@gmail.com>
2023-10-04 17:09:45 -0500

    Swtiched Repo from GetKraken to Here
    No major changes done

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -16,24 +16,30 @@ namespace HelloDungeon
         //JJ Debug: became an issue with 'input' variable on return type being either int or string. decided to separate them into...
         // 'int inputNumber = 0;' and 'string inputText = "";' Plan to impliment these in place of the variable 'input'.
         // This I hope will take away the problem of too many defined local 'input' variables.
+       
+        //JJ Debug: 11SEP23 changed 'bool gameOver = false;' to 'bool gameOver = true;' . I think the while loop needs something
+        //gonna change 'currentArea = -1;' to 'currentArea = 1' hoping this will run the while loop.
+        //It works as intended from what I can see at this point. 
         string characterName = "";
-        float currentArea = -1;
-        bool gameOver = true;
+        float currentArea = 1;
+        bool gameOver = false;
         float health = 20;
         bool playerIsAlive = false;
         int inputNumber = 0;
-        string inputText = " ";
+        string inputText = "";
         /// <summary>
         /// The starting room where the player gives their name, and has their first encounter.
         /// </summary>
         void Room1()
         {
             //JJ debug: Pretty sure Console.Write will only Print 1 character so changed first line to Console.WriteLine.
+            //adding readkey so it doesn't skip Hello statement at the Console.Clear.
             ///Get the name from the player
             Console.WriteLine("Please enter your name.");
+            health = 20;
             characterName = Console.ReadLine();
             Console.WriteLine("Hello, " + characterName);
-
+            Console.ReadKey(true);
             Console.Clear();
             // JJ Debug: changed initial line to 'string input =' instead of int input =. not sure how to debug yet.
             // JJ Debug: in if statement changed 'if/else if (input =' to 'if/else if (input =='. this seemed to fix error, but...
@@ -42,22 +48,28 @@ namespace HelloDungeon
             //JJ Debug: noticed further with some help from classmate that the function 'GetInput' had an output set to 'void'...
             // changed it lower down to 'int' instead so it could output an integer. have yet to run program to see if this works.
 
+            //JJ Debug: changed input to inputText and made it all strings to make the function work. believe using int will not work.
+            //JJ Debug flipped yes and no options because they were displaying out of order for the desired result for the player.
             ///Display text for the first encounter, and store the players decision
-            int inputNumber = GetInput("You've been approached by a traveler!! " +
-                "\n They offer you a potion. Do you accept?","No", "Yes" );
+            string inputText = GetInput("You've been approached by a traveler!! " +
+                "\n They offer you a potion. Do you accept?","Yes", "No" );
            
             ///If the player drinks the potion...
-            if (inputNumber == 1)
+            if (inputText == "1")
             {
                 ///...kill the player
                 Console.WriteLine("It was posion!! Ya dead shuuuunnnnn");
                 playerIsAlive = false;
             }
             ///Otherwise if they do not...
-            else if (inputNumber == 2)
+            else if (inputText == "2")
             {
                 ///...display text to let the player know that they survived the first room
+                //JJ Debug: added ReadKey to not auto skip text
+                //JJ Debug: added currentArea++ to see if this makes the next room play from the while loop. this worked.
                 Console.WriteLine("You decide to follow your gut and decline. You move on to the next area.");
+                Console.ReadKey(true);
+                currentArea++;
             }
         }
 
@@ -133,23 +145,27 @@ namespace HelloDungeon
                 if (health <= 0)
                 {
                     ///...update the player state and print player feedback to the screen
+                    //added return; to break it from the loop and see if this will display gameover sequence
                     playerIsAlive = false;
                     Console.WriteLine("You died...");
                     Console.ReadKey();
                     Console.Clear();
+                    return;
                 }
             }
         }
         /// <summary>
         /// Prints the menu for restarting or exiting the game
         /// </summary>
+        //JJ Debug: changed input to inputText and made it all strings to work with while loop function below after changes made...
+        // to get it to stop looping forever.
         void DisplayMainMenu()
         {
             ///Display question and store input
-            int input = GetInput("Would you like to play again?", "Yes", "No");
+            string inputText = GetInput("Would you like to play again?", "Yes", "No");
 
             ///If the player decides to restart...
-            if (input == 1)
+            if (inputText == "1")
             {
                 ///...set their current area to be the start and update the player state to be alive
                 currentArea = 1;
@@ -157,7 +173,7 @@ namespace HelloDungeon
                 playerIsAlive = true;
             }
             ///Otherwise if the player wants to quit...
-            else if (input == 2)
+            else if (inputText == "2")
             {
                 //...set game over to be true
                 gameOver = true;
@@ -185,34 +201,43 @@ namespace HelloDungeon
         //JJ Debug: unsure if i should define more variables in higher scope or leave local variables such as 'inputReceived' as is.
         // will impliment 'inputText' for now. also unsure if putting 'string inputText' here defines it as a local variable or reference
         // to the global variable I made earlier.
-        int GetInput(string description, string option1, string option2)
+        //Changed int GetInput to string GetInput beacause string makes more sense for the output needed for the function.
+        string GetInput(string description, string option1, string option2)
         {
             string inputText = "";
-            int inputReceived = 0;
+            string inputReceived = "";
 
             ///While input is not 1 or 2 display the options
-            while (!(inputReceived == 1 && inputReceived == 2))
+            //changed inputReceived from 'int' to 'string'
+            //while (!(inputText != "1" && inputText != "2")) changed to while (inputText != "1" && inputText != "2") because
+            //unnecessary syntax.
+            while (inputText != "1" && inputText != "2")
             {
                 ///Print options
-                Console.Write(description);
-                Console.Write("1. " + option1);
-                Console.Write("2. " + option2);
+                //JJ Debug: changing 'Console.Write'  below for all the options and description to 'Console.WriteLine'...
+                // that way all the text doesn't bunch up.
+                Console.WriteLine(description);
+                Console.WriteLine("1. " + option1);
+                Console.WriteLine("2. " + option2);
                 Console.Write("> ");
 
                 ///Get input from player
                 inputText = Console.ReadLine();
+                
+                //JJ Debug: changing != for both parts of the initial if statement to == because != doesn't make sense to me.
+                //JJ Debug: changed 2nd if statement to else if to connect them or however you say it. 
 
                 ///If player selected the first option...
-                if (inputText != "1" || inputText != option1)
+                if (inputText == "1" || inputText == option1)
                 {
                     ///Set input received to be the first option
-                    inputReceived = 1;
+                    inputReceived = "1";
                 }
                 ///Otherwise if the player selected the second option...
-                if (inputText == "2" && inputText == option2)
+                else if (inputText == "2" || inputText == option2)
                 {
                     ///Set input received to be the second option
-                    inputReceived = 2;
+                    inputReceived = "2";
                 }
                 ///If neither are true...
                 else
@@ -231,22 +256,24 @@ namespace HelloDungeon
         public void Run()
         {
             ///Loop while game isn't over
-            while (gameOver)
+            ///penis
+            while (gameOver == false)
             {
                 ///Print the current room to the screen
-                if (currentArea >= 1)
+                //JJ Debug changed CurrentArea >= to == hoping that allows it to increment the room number. def 
+                if (currentArea == 1)
                 {
                     Room1();
                 }
-                if (currentArea >= 2)
+                if (currentArea == 2)
                 {
                     Room2();
                 }
-                if (currentArea >= 3)
+                if (currentArea == 3)
                 {
                     Room3();
                 }
-            }
+
                 ///If the player lost or beat the game...
                 if (playerIsAlive == false || currentArea == 3)
                 {
@@ -254,12 +281,13 @@ namespace HelloDungeon
                     DisplayMainMenu();
                 }
                 ///Otherwise if the player is alive and hasn't finished...
+               
                 else
                 {
                     ///...increment the current area
                     currentArea++;
                 }
-            
-        }
+            }
+        }   
     }
 }
